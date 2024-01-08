3819b464747c730e1e3ea4f235edeefd4982ddf2
Sujeet Poudel (Contractor) <Sujeet.Poudel@albertsons.com>
2024-01-08 12:03:17 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Sujeet Poudel (Contractor) on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
